b6b138eabd9b5ed3d64217fc02be61f1b98194c7
sabda <sabdasetiawan206@student.uri.ac.id>
2024-03-14 22:36:11 +0700

    Add : navbar, hero, main containe

---
+++ src/components/Home/Main/Main.tsx
@@ -0,0 +1,70 @@
+import Link from 'next/link';
+import { AiFillAndroid } from 'react-icons/ai';
+import { MdOutlineWeb } from 'react-icons/md';
+import { GrCodeSandbox } from 'react-icons/gr';
+const Main = () => {
+  return (
+    <div className=" w-[80%] h-[480px] grid grid-cols-2 mt-16 ">
+      <section className="flex items-center justify-center">
+        <div className=" w-[380px] h-[457px] bg-[url('/Images/main.png')] bg-contain flex  rounded-md relative ">
+          <Link
+            href={'/'}
+            className=" absolute top-[1.4rem] rounded-tr-lg shadow-lg  right-6 bg-white p-4 text-[1rem]"
+          >
+            View Detail
+          </Link>
+        </div>
+      </section>
+      <section className="">
+        <h2 className=" text-[2rem] ml-[80px] font-semibold text-[#414141]">
+          What We Do ?
+        </h2>
+
+        <div className="main-content flex flex-col  gap-y-3 mt-5">
+          <section className="flex gap-x-8 ">
+            <AiFillAndroid size={28} />
+            <div>
+              <h3 className="text-[0.9rem]  text-[#414141] font-semibold">
+                Mobile Apps
+              </h3>
+              <p className="text-[0.7rem] w-[60%] text-[#5E5B5B]">
+                Building mobile applications is never easy. P2P lending app ?
+                Logistic App ? E-Commerce App ? We have done them all. Done them
+                all well. Done them all fast.{' '}
+              </p>
+            </div>
+          </section>
+          <section className="flex gap-x-8 ">
+            <MdOutlineWeb size={28} />
+            <div>
+              <h3 className="text-[0.9rem]  text-[#414141] font-semibold">
+                Web Application
+              </h3>
+              <p className="text-[0.7rem] w-[60%] text-[#5E5B5B]">
+                We can build any web applications you need. A personal blog ?
+                Company Profile ? E-commerce ? or any specific functionalities ?
+                We got you covered! We have a strong team of web developers that
+                can follow your stack and needs.
+              </p>
+            </div>
+          </section>
+          <section className="flex gap-x-8 ">
+            <GrCodeSandbox size={28} />
+            <div>
+              <h3 className="text-[0.9rem]  text-[#414141] font-semibold">
+                UI/UX Design
+              </h3>
+              <p className="text-[0.7rem] w-[60%] text-[#5E5B5B]">
+                All things are designed. A few things are designed well. We try
+                to elevate your ideas with great UI/UX design, suited to your
+                taste, addressing the right pain points.
+              </p>
+            </div>
+          </section>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Main;
---
+++ src/components/Home/Navbar/Navbar.tsx
@@ -0,0 +1,78 @@
+'use client';
+
+import Style from './navbar.module.css';
+import Image from 'next/image';
+import Link from 'next/link';
+import { FiSearch } from 'react-icons/fi';
+import { TiThMenuOutline } from 'react-icons/ti';
+import { useState } from 'react';
+
+const Navbar = () => {
+  const [menu, setMenu] = useState(false);
+
+  return (
+    <div className=" container shadow-md  w-screen h-[100px] flex justify-between items-center  mx-auto box-border">
+      <div className={Style.container__img}>
+        <Image
+          src={'/Images/Logo.png'}
+          alt="logo Whatsabb"
+          fill
+          objectFit="cover"
+          objectPosition="center"
+        />
+      </div>
+      <div className={Style.container__item}>
+        <Link href={'/'}>
+          <p className={Style.item}>Home</p>
+        </Link>
+        <Link href={'/'}>
+          <p className={Style.item}>Service</p>
+        </Link>
+        <Link href={'/'}>
+          <p className={Style.item}>Our Customer</p>
+        </Link>
+        <Link href={'/'}>
+          <p className={Style.item}>Team</p>
+        </Link>
+        <Link href={'/'}>
+          <p className={Style.item}>Career</p>
+        </Link>
+        <Link href={'/'}>
+          <p className={Style.item}>Contact</p>
+        </Link>
+        <FiSearch size={20} className=" ml-9" />
+      </div>
+      <TiThMenuOutline
+        size={25}
+        className={Style.menu}
+        onClick={() => setMenu(!menu)}
+      />
+      <div
+        className={` ${
+          menu ? 'translate-x-0' : ' -translate-x-full'
+        }  absolute transition-all ease-in-out delay-300 right-0 top-[6.4rem]  bg-gray-100/80 gap-6 flex flex-col items-center w-screen h-[85vh]`}
+      >
+        <Link href={'/'}>
+          <p className={Style.item}>Home</p>
+        </Link>
+        <Link href={'/'}>
+          <p className={Style.item}>Service</p>
+        </Link>
+        <Link href={'/'}>
+          <p className={Style.item}>Our Customer</p>
+        </Link>
+        <Link href={'/'}>
+          <p className={Style.item}>Team</p>
+        </Link>
+        <Link href={'/'}>
+          <p className={Style.item}>Career</p>
+        </Link>
+        <Link href={'/'}>
+          <p className={Style.item}>Contact</p>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
