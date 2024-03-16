c6cd73a878b9940348b13bc35bb77f2dc1529537
Rahmat Ardiansyah <artsbymat@gmail.com>
2024-03-16 20:10:44 +0700

    refactor: refactor navbar item

--- src/components/Home/Navbar/Navbar.tsx
+++ src/components/Home/Navbar/Navbar.tsx
@@ -7,11 +7,20 @@ import { FiSearch } from 'react-icons/fi';
 import { TiThMenuOutline } from 'react-icons/ti';
 import { useState } from 'react';
 
+const navItem = [
+  { name: 'Home', link: '/' },
+  { name: 'Service', link: '/' },
+  { name: 'Our Customer', link: '/' },
+  { name: 'Team', link: '/' },
+  { name: 'Career', link: '/' },
+  { name: 'Contact', link: '/' }
+];
+
 const Navbar = () => {
   const [menu, setMenu] = useState(false);
 
   return (
-    <div className=" container shadow-md  w-screen h-[100px] flex justify-between items-center  mx-auto box-border">
+    <div className="container w-screen h-[100px] flex justify-between items-center  mx-auto box-border">
       <div className={Style.container__img}>
         <Image
           src={'/Images/logo.png'}
@@ -21,54 +30,24 @@ const Navbar = () => {
         />
       </div>
       <div className={Style.container__item}>
-        <Link href={'/'}>
-          <p className={Style.item}>Home</p>
-        </Link>
-        <Link href={'/'}>
-          <p className={Style.item}>Service</p>
-        </Link>
-        <Link href={'/'}>
-          <p className={Style.item}>Our Customer</p>
-        </Link>
-        <Link href={'/'}>
-          <p className={Style.item}>Team</p>
-        </Link>
-        <Link href={'/'}>
-          <p className={Style.item}>Career</p>
-        </Link>
-        <Link href={'/'}>
-          <p className={Style.item}>Contact</p>
-        </Link>
+        {navItem.map((item) => (
+          <Link key={item.name} href={item.link}>
+            <p className={Style.item}>{item.name}</p>
+          </Link>
+        ))}
         <FiSearch size={20} className=" ml-9" />
       </div>
-      <TiThMenuOutline
-        size={25}
-        className={Style.menu}
-        onClick={() => setMenu(!menu)}
-      />
+      <TiThMenuOutline size={25} className={Style.menu} onClick={() => setMenu(!menu)} />
       <div
         className={` ${
           menu ? 'translate-x-0' : ' -translate-x-full'
         }  absolute transition-all ease-in-out delay-300 right-0 top-[6.4rem]  bg-gray-100/80 gap-6 flex flex-col items-center w-screen h-[85vh]`}
       >
-        <Link href={'/'}>
-          <p className={Style.item}>Home</p>
-        </Link>
-        <Link href={'/'}>
-          <p className={Style.item}>Service</p>
-        </Link>
-        <Link href={'/'}>
-          <p className={Style.item}>Our Customer</p>
-        </Link>
-        <Link href={'/'}>
-          <p className={Style.item}>Team</p>
-        </Link>
-        <Link href={'/'}>
-          <p className={Style.item}>Career</p>
-        </Link>
-        <Link href={'/'}>
-          <p className={Style.item}>Contact</p>
-        </Link>
+        {navItem.map((item) => (
+          <Link key={item.name} href={item.link}>
+            <p className={Style.item}>{item.name}</p>
+          </Link>
+        ))}
       </div>
     </div>
   );
