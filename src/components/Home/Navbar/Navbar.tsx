1822236a3330d6c8afa4d84e243c2368c704d361
Rahmat Ardiansyah <artsbymat@gmail.com>
2024-03-17 03:46:52 +0700

    feat & style: menambahkan link active dan mengubah design navbar

--- src/components/Home/Navbar/Navbar.tsx
+++ src/components/Home/Navbar/Navbar.tsx
@@ -6,19 +6,20 @@ import Link from 'next/link';
 import { FiSearch } from 'react-icons/fi';
 import { TiThMenuOutline } from 'react-icons/ti';
 import { useState } from 'react';
+import { usePathname } from 'next/navigation';
 
 const navItem = [
   { name: 'Home', link: '/' },
-  { name: 'Service', link: '/' },
-  { name: 'Our Customer', link: '/' },
-  { name: 'Team', link: '/' },
-  { name: 'Career', link: '/' },
-  { name: 'Contact', link: '/' }
+  { name: 'Service', link: '/service' },
+  { name: 'Our Customer', link: '/customer' },
+  { name: 'Team', link: '/team' },
+  { name: 'Career', link: '/career' },
+  { name: 'Contact', link: '/contact' }
 ];
 
 const Navbar = () => {
   const [menu, setMenu] = useState<boolean>(false);
-  const [active, setActive] = useState<string>('');
+  const pathname = usePathname();
 
   return (
     <div className="container w-screen h-[100px] flex justify-between items-center  mx-auto box-border">
@@ -32,12 +33,8 @@ const Navbar = () => {
       </div>
       <div className={Style.container__item}>
         {navItem.map((item) => (
-          <Link key={item.name} href={item.link} onClick={() => setActive(item.name)}>
-            <p
-              className={`${Style.item} ${active === item.name ? 'border-b-2 border-black' : ''} `}
-            >
-              {item.name}
-            </p>
+          <Link key={item.name} href={item.link}>
+            <p className={`${item.link === pathname ? 'border-b-2 border-black' : ''}`}>{item.name}</p>
           </Link>
         ))}
         <FiSearch size={20} className=" ml-9" />
@@ -46,11 +43,11 @@ const Navbar = () => {
       <div
         className={` ${
           menu ? 'translate-x-0' : ' -translate-x-full'
-        }  absolute transition-all ease-in-out delay-300 right-0 top-[6.4rem]  bg-gray-100/80 gap-6 flex flex-col items-center w-screen h-[85vh]`}
+        } absolute transition-all ease-in-out delay-300 right-0 top-0 bg-gray-100/80 gap-6 flex flex-col items-center w-screen h-screen justify-center backdrop-blur-sm z-10`}
       >
         {navItem.map((item) => (
           <Link key={item.name} href={item.link}>
-            <p className={Style.item}>{item.name}</p>
+            <p className={`${item.link === pathname ? 'border-b-2 border-black' : ''}`}>{item.name}</p>
           </Link>
         ))}
       </div>
