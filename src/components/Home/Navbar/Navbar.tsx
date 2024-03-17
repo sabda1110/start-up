6ce15b2b0e0ee00e6a89b4a428688c097499c99e
Rahmat Ardiansyah <artsbymat@gmail.com>
2024-03-17 16:32:47 +0700

    fix: memperbaiki navbar

--- src/components/Home/Navbar/Navbar.tsx
+++ src/components/Home/Navbar/Navbar.tsx
@@ -21,33 +21,42 @@ const Navbar = () => {
   const [menu, setMenu] = useState<boolean>(false);
   const pathname = usePathname();
 
+  const handleClick = () => {
+    setMenu(!menu);
+    document.body.classList.toggle('overflow-hidden');
+  };
+
   return (
     <div className="container w-screen h-[100px] flex justify-between items-center  mx-auto box-border">
-      <div className={Style.container__img}>
+      <Link href="/" className={Style.container__img}>
         <Image
           src={'/Images/logo.png'}
           alt="logo Whatsabb"
           fill
           style={{ objectFit: 'cover', objectPosition: 'center' }}
         />
-      </div>
+      </Link>
       <div className={Style.container__item}>
         {navItem.map((item) => (
           <Link key={item.name} href={item.link}>
-            <p className={`${item.link === pathname ? 'border-b-2 border-black' : ''}`}>{item.name}</p>
+            <p className={`${item.link === pathname ? 'border-b-2 border-black' : ''}`}>
+              {item.name}
+            </p>
           </Link>
         ))}
         <FiSearch size={20} className=" ml-9" />
       </div>
-      <TiThMenuOutline size={25} className={Style.menu} onClick={() => setMenu(!menu)} />
+      <TiThMenuOutline size={25} className={Style.menu} onClick={handleClick} />
       <div
         className={` ${
           menu ? 'translate-x-0' : ' -translate-x-full'
-        } absolute transition-all ease-in-out delay-300 right-0 top-0 bg-gray-100/80 gap-6 flex flex-col items-center w-screen h-screen justify-center backdrop-blur-sm z-10`}
+        } lg:hidden absolute transition-all ease-in-out delay-300 right-0 top-0 bg-gray-100/80 gap-6 flex flex-col items-center w-screen h-screen justify-center backdrop-blur-sm z-10`}
       >
         {navItem.map((item) => (
           <Link key={item.name} href={item.link}>
-            <p className={`${item.link === pathname ? 'border-b-2 border-black' : ''}`}>{item.name}</p>
+            <p className={`${item.link === pathname ? 'border-b-2 border-black' : ''}`}>
+              {item.name}
+            </p>
           </Link>
         ))}
       </div>
